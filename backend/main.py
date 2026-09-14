aa4f195f3dd8ff254056078fdf5a9daeda24dbfd
Yousif Jbour <yousif.samir.jbour@gmail.com>
2026-09-14 19:52:59 +0300

    Add AWS IP to backend CORS origins

--- backend/main.py
+++ backend/main.py
@@ -20,12 +20,11 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=["http://localhost:5173", "http://13.62.51.232:5173"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 # Authentication Routers
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
