70027f79969f97f4f2c841145220dc2ad5c7d85e
Yousif Jbour <yousif.samir.jbour@gmail.com>
2026-09-14 23:08:43 +0300

    Allow DuckDNS host in Vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: [
+      'socialfeed.duckdns.org'
+    ]
+  }
 })
